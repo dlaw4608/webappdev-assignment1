e428dbb6f3072810c97089a30a7add4ef75e6602
dlaw4608 <dlawton032@gmail.com>
2023-11-27 03:52:43 +0000

    got forward and backward arrows working

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -101,6 +101,22 @@ export const getMovie = (args) => {
     });
   };
 
+  export const getSimilarMovies = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    ).then( (response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+  
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
 
   export const getTvShows = ({queryKey}) => {
     const [, idPart] = queryKey;
